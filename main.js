5e02c6cb8acc73ca107750f959e3d1095adb1407
Vitor Gonçalves Piovezam <vitorpiovezam@yandex.com>
2019-08-04 19:32:39 -0300

    heroku port fix

--- main.js
+++ main.js
@@ -4,15 +4,17 @@ const bodyParser = require('body-parser');
 const puppeteer = require('puppeteer');
 
 async function run() {
-    const app = express();
-    const port = process.env.port || 8080;
     app.use(cors());
+
+    const app = express();
     let url = 'https://www.pensador.com/frases_de_bob_marley/';
     
     const clearText = (text) => text.replace(/<\/?[^>]+(>|$)/g, "");
     const sortPhrase = (itemsList) => itemsList[Math.floor(Math.random() * itemsList.length)];
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox']
+    });
     const page = await browser.newPage();
     await page.goto(url);
 
@@ -24,7 +26,7 @@ async function run() {
         res.json(clearText(frase));
     });
 
-    app.listen(port, () => {
+    app.listen(process.env.PORT || 8080, () => {
         console.log('🔥' + port);
     });
 };
